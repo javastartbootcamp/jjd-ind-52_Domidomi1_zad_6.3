9eca462c7a61b78cf59c15a051701a4df72e2ac7
Domidomi1 <dominika.wen@gmail.com>
2023-04-03 19:31:58 +0200

    solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,25 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
+    @SuppressWarnings({"checkstyle:WhitespaceAfter", "checkstyle:NeedBraces"})
     public static void main(String[] args) {
+
+        int[][] tab = new int[100][100];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj pierwszy wymiar tablicy: ");
+        int a = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Podaj drugi wymiar tablicy: ");
+        int b = scanner.nextInt();
+        scanner.nextLine();
+
+        tab[a][b] = a * b;
+
+        System.out.println(("Tablica: " + Arrays.toString(tab[a]) + " " + Arrays.toString(tab[b])));
 
     }
 }
